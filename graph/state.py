17ff602919918029f17821df5cc3834a54e763cb
Vinicius Finger <fingervinicius@icloud.com>
2026-05-05 08:09:43 -0300

    Pass the necessary data to the graph

--- graph/state.py
+++ graph/state.py
@@ -2,5 +2,11 @@ from typing import Annotated, TypedDict
 from langchain.messages import AnyMessage
 import operator
 
+from domain.schema.urgency import Urgency
+
 class GraphState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
+    age: int
+    comorbidity: list[str]
+    surgical_type: str
+    urgency: Urgency
